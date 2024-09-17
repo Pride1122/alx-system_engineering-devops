84c1db3b6a857b9d83caf7b31b3601d077de7c30
Pride1122 <icekidomachonu@gmail.com>
2024-09-17 23:36:11 +0000

    files

--- 0x1B-web_stack_debugging_4/0-the_sky_is_the_limit_not.pp
+++ 0x1B-web_stack_debugging_4/0-the_sky_is_the_limit_not.pp
@@ -1,5 +1,13 @@
-# puppet code - use strace to find out why apache is returning a 500 error
+# This manuscript increases the amount of traffic an Nginx server can handle
 
-exec { 'fix_error':
-  command => "/bin/sed -i 's/15/4096/g' /etc/default/nginx; ; sudo service nginx restart",
+# Increase the ULIMIT of the default file
+exec { 'fix--for-nginx':
+  command => 'sed -i "s/15/4096/" /etc/default/nginx',
+  path    => '/usr/local/bin/:/bin/'
+}
+
+# Restart Nginx
+-> exec { 'nginx-restart':
+  command => 'nginx restart',
+  path    => '/etc/init.d/'
 }
--- 0x1B-web_stack_debugging_4/1-user_limit.pp
+++ 0x1B-web_stack_debugging_4/1-user_limit.pp
@@ -1,4 +1,13 @@
-#fix os error change config
-exec { 'change_config':
-  command => "sudo /bin/sed -i 's/nofile 4/nofile 6000/g; s/nofile 5/nofile 6000/g' /etc/security/limits.conf",
+# This manuscript enables the user holberton to login and open files without error
+
+# Increase hard file limit for user holberton
+exec { 'increase-hard-file-limit-for-holberton-user':
+  command => 'sed -i "/holberton hard/s/5/50000/" /etc/security/limits.conf',
+  path    => '/usr/local/bin/:/bin/'
+}
+
+# Increase soft file limit for user holberton
+exec { 'increase-soft-file-limit-for-holberton-user':
+  command => 'sed -i "/holberton soft/s/4/50000/" /etc/security/limits.conf',
+  path    => '/usr/local/bin/:/bin/'
 }
